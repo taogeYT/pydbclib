0c80e6741b43dc039b7645f008bbd319c50de5e8
liyatao <li_yatao@outlook.com>
2020-09-13 00:41:19 +0800

    版本更新2.2

--- pydbclib/__init__.py
+++ pydbclib/__init__.py
@@ -6,11 +6,12 @@ from pydbclib.database import Database
 from pydbclib.drivers import CommonDriver, SQLAlchemyDriver
 
 __author__ = "liyatao"
-__version__ = '2.1.8'
+__version__ = '2.2'
 
 
 def connect(*args, **kwargs):
     driver = kwargs.get("driver", "sqlalchemy")
+    kwargs.update(driver=driver)
     if isinstance(driver, str):
         driver_class = {"sqlalchemy": SQLAlchemyDriver}.get(driver.lower(), CommonDriver)
     elif hasattr(driver, "cursor"):
